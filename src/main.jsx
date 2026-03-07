86a4543ece0658f69572de3d3c19ab177a5dec5f
KBuzin <36828671+KBuzin@users.noreply.github.com>
2026-03-07 06:48:51 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WalkerScore from './WalkerScore.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WalkerScore />
+  </React.StrictMode>,
+)
